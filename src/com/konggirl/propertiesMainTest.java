7ffdc67ec53f7749229b0cf241bfb04a1d9c18db
konggirl <http://www.github.com/>
2017-01-25 11:42:47 +0900

    #UPLOAD 01/25/2017

--- src/com/konggirl/propertiesMainTest.java
+++ src/com/konggirl/propertiesMainTest.java
@@ -5,10 +5,12 @@ import com.konggirl.common.utils.PropertiesUtil;
 public class propertiesMainTest {
 
 	private static final String propTest = PropertiesUtil.getStringProperties("local", "properties.test");
+	private static final int propInt = PropertiesUtil.getIntProperties("local", "properties.int.test");
 	
 	public static void main(String[] args) {
 		
 		System.out.println("properties.test : " + propTest);
+		System.out.println("properties.int.test : " + propInt);
 	}
 
 }
